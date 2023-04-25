dab91fdef5bb42970ec08132e979dd91b45c7581
Cindy <karanjacindy1@gmail.com>
2023-04-25 11:59:57 -0700

    functions

--- functions.c
+++ functions.c
@@ -138,3 +138,48 @@ int print_int(va_list types, char buffer[], int flags, int width,
 	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
+/**
+ * print_binary - Prints an unsigned number
+ * @types: Arguments list
+ * @buffer: Buffer array
+ * @flags: Calculates active flags
+ * @width: width
+ * @precision: Precision specification
+ * @size: size specification
+ * Return: Number of characters printed
+ */
+
+int print_binary(va_list types, char buffer[], int flags, int width,
+		int precision, int size)
+{
+	unsigned int n, m, i, sum, a[32], count;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	n = va_arg(types, unsigned int);
+	m = 2147483648;
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	sum = 0;
+	count = 0;
+	for (i = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
+}
